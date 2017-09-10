b5f8b1ba0b3c13785ea6c8ef56fcbfcf8fd539cc
ESAERO\NickB <nick.brake@esaero.com>
2017-09-10 10:38:33 -0700

    Increase CAN spin time from 1msec to 10msec

--- libraries/AP_UAVCAN/AP_UAVCAN.cpp
+++ libraries/AP_UAVCAN/AP_UAVCAN.cpp
@@ -401,7 +401,7 @@ void AP_UAVCAN::do_cyclic(void)
 
     if (_initialized) {
         auto *node = get_node();
-        const int error = node->spin(uavcan::MonotonicDuration::fromMSec(1));
+        const int error = node->spin(uavcan::MonotonicDuration::fromMSec(10));
         if (error < 0) {
             hal.scheduler->delay_microseconds(1000);
         } else {
